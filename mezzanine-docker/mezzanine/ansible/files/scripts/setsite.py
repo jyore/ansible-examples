cd01fc1c0e249c63017b76ec8199b80f419b26ba
jyore <joey@jyore.com>
2016-01-29 19:12:24 -0600

    commenting code, pending removal

--- mezzanine-docker/mezzanine/ansible/files/scripts/setsite.py
+++ mezzanine-docker/mezzanine/ansible/files/scripts/setsite.py
@@ -2,6 +2,7 @@
 import os
 import sys
 
+"""
 proj_dir = os.path.expanduser(os.environ['PROJECT_DIR'])
 sys.path.append(proj_dir)
 
@@ -13,3 +14,4 @@ from django.contrib.sites.models import Site
 domain = os.environ['WEBSITE_DOMAIN']
 Site.objects.filter(id=settings.SITE_ID).update(domain=domain)
 Site.objects.get_or_create(domain=domain)
+"""
